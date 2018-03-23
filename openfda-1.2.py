b9f4cfc03a18b4b70f373df3ec26629b6ce281bf
v.sanchezga.2017 <viole1998@gmail.com>
2018-03-23 16:09:03 +0100

    openfda-1.2

---
+++ openfda-1.2.py
@@ -0,0 +1,18 @@
+import http.client
+import json
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/event.json", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+
+repo = repos['results']
+
+for element in repo:
+    print("Another 10 drugs are the following:", repo[0]['patient']['drug'][3]['openfda']['spl_id'][0:10])
