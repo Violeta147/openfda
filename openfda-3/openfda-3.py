ccccfb3957f22832406b36c40d4b9eb345ae8808
v.sanchezga.2017 <viole1998@gmail.com>
2018-04-10 16:25:59 +0200

    openfda-3

---
+++ openfda-3/openfda-3.py
@@ -0,0 +1,31 @@
+import http.server
+import socketserver
+
+socketserver.TCPServer.allow_reuse_address = True
+
+PORT = 8002
+
+# HTTPRequestHandler class
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    # GET
+    def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        with open ("search.html") as file_search:
+            message = file_search.read()
+            self.wfile.write(bytes(message, "utf8"))
+            return
+
+Handler = http.server.SimpleHTTPRequestHandler
+Handler = testHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
+
+# https://github.com/joshmaker/simple-python-webserver/blob/master/server.py
